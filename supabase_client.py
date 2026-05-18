c25fc3a4922f83b76e541691773fe621e9bbb47e
joei751007-prog <joei751007@gmail.com>
2026-05-18 23:03:05 +0800

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,6 +1,7 @@
+import os
 from supabase import create_client
 
-SUPABASE_URL = "你的URL"
-SUPABASE_KEY = "你的anon key"
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
